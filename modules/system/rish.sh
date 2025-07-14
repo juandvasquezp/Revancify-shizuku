ecc5bb9dd5a436da856633cc972e79c7dd8d882d
Juan David <47906946+juandvasquezp@users.noreply.github.com>
2025-07-13 23:41:04 -0500

    Rish (#5)
    Version 2 with secure folder verification.
    
    The only usecase that I couldn't fix was when using the patch "chang packag name" and installing two apps from the same repo but with different package names. It's not something I wanna think about, and the logs are clear if someone tries to do it.

--- modules/system/rish.sh
+++ modules/system/rish.sh
@@ -18,24 +18,46 @@ installAppRish() {
     # Case 1 is literally the worst, and there's even cases where we cannot know if there's a hidden installation or not, so we need to ask the user if they want to uninstall the app from all users
 
     notify info "Please Wait !!\nInstalling $APP_NAME using Rish..."
+
+    # Obtain the pkg name and version of the patched APK
+    # We also obtain the version because there is a patch that can change the version of the APK.
+    local PATCHED_APP_INFO
+    if ! PATCHED_APP_INFO=$(./bin/aapt2 dump badging "apps/$APP_NAME/$APP_VER-$SOURCE.apk"); then
+        notify msg "The patched Apk is not valid. Patch again and retry."
+        return 1
+    fi
+    PATCHED_APP_PKG_NAME=$(grep -oP "(?<=package: name=')[^']+" <<< "$PATCHED_APP_INFO")
+    local PATCHED_APP_APP_NAME=$(grep -oP "(?<=application-label:')[^']+" <<< "$PATCHED_APP_INFO" | sed -E 's/[.: ]+/-/g')
+    local PATCHED_APP_VERSION=$(grep -oP "(?<=versionName=')[^']+" <<< "$PATCHED_APP_INFO")
+
+    log "Patched APK info: Package Name: $PATCHED_APP_PKG_NAME, App Name: $PATCHED_APP_APP_NAME, Version: $PATCHED_APP_VERSION"
+
+
+    if [ "$PATCHED_APP_PKG_NAME" != "$PKG_NAME" ]; then
+        log "Package name mismatch: $PATCHED_APP_PKG_NAME != $PKG_NAME, selected APK has a different package name than patched apk."
+    fi
+
     # Copy the patched APK to the storage
     CANONICAL_VER=${APP_VER//:/}
     local EXPORTED_APK_NAME="$APP_NAME-$CANONICAL_VER-$SOURCE"
     cp -f "apps/$APP_NAME/$APP_VER-$SOURCE.apk" "$STORAGE/Patched/$EXPORTED_APK_NAME.apk" &> /dev/null
-    
+
     # Verify current installed version and signatures
-    log "Checking if $PKG_NAME is installed"
-    getInstalledVersion
-    if [ "$INSTALLED_VERSION" != "" ]; then
-        log "Installed version of $APP_NAME is $INSTALLED_VERSION"
+    log "Checking if $PATCHED_APP_PKG_NAME is installed"
+    # getInstalledVersion
+    # We use th rish command instead
+    local INSTALLED_PATCHED_VERSION=$(rish -c "dumpsys package $PATCHED_APP_PKG_NAME" | sed -n '/versionName/s/.*=//p' | sed -n '1p')
+
+    if [ "$INSTALLED_PATCHED_VERSION" != "" ]; then
+        log "Installed version of $PATCHED_APP_APP_NAME is $INSTALLED_PATCHED_VERSION"
         log "Verifying signatures..."
         local STOCK_APP_PATH
-        if [ "$(rish -c "pm list packages --user current | grep -q $PKG_NAME && echo Installed")" == "Installed" ]; then
-            STOCK_APP_PATH=$(rish -c "pm path --user current $PKG_NAME | sed -n '/base/s/package://p'")
+        if [ "$(rish -c "pm list packages --user current | grep -q $PATCHED_APP_PKG_NAME && echo Installed")" == "Installed" ]; then
+            STOCK_APP_PATH=$(rish -c "pm path --user current $PATCHED_APP_PKG_NAME | sed -n '/base/s/package://p'")
         else
             # If the app is not installed in the current user, we try to get the stock app path from dumpsys
             # This means the app is installed in a different user
-            STOCK_APP_PATH=$(rish -c "dumpsys package $PKG_NAME | sed -n 's/^[[:space:]]*path: \(.*base\.apk\)/\1/p'")
+            STOCK_APP_PATH=$(rish -c "dumpsys package $PATCHED_APP_PKG_NAME | sed -n 's/^[[:space:]]*path: \(.*base\.apk\)/\1/p'")
             log "Dumpsys used to get stock app path, that means the app is installed but in a different user."
             HIDDEN_APP_INSTALL=true
         fi
@@ -61,7 +83,7 @@ installAppRish() {
                 else
                     # User declined to uninstall the current app for Case 2
                     log "Case 2: User declined to uninstall the current app."
-                    notify msg "User declined to uninstall the current app.\n\nAborting installation...\n\nCopied patched $APP_NAME apk to Internal Storage..."
+                    notify msg "User declined to uninstall the current app.\n\nAborting installation...\n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
                     return 1
                 fi
             fi
@@ -69,33 +91,33 @@ installAppRish() {
             log "Signature match, we can upgrade the app."
         fi
     else
-        log "No installed version found for $APP_NAME found, proceeding with installation."
+        log "No installed version found for $PATCHED_APP_APP_NAME found, proceeding with installation."
     fi
 
     # Check if we're already due for uninstallation
     if [ "$UNINSTALL_CURRENT_INSTALLATION" == false ]; then
         log "Checking if it's a downgrade..."
-        if jq -e '.[0] > .[1]' <<< "[\"${INSTALLED_VERSION:-0}\", \"$APP_VER\"]" &> /dev/null; then
+        if jq -e '.[0] > .[1]' <<< "[\"${INSTALLED_PATCHED_VERSION:-0}\", \"$PATCHED_APP_VERSION\"]" &> /dev/null; then
             # Case 3: Installed version is greater than the new version, we are downgrading
             # We need to uninstall the current app first, from current user, if it fails we can try to uninstall from all users
-            log "Case 3: Installed version $INSTALLED_VERSION is greater than the new version $APP_VER, we are downgrading."
+            log "Case 3: Installed version $INSTALLED_PATCHED_VERSION is greater than the new version $PATCHED_APP_VERSION, we are downgrading."
             if [ "$ALLOW_APP_VERSION_DOWNGRADE" == "on" ]; then
                 log "Case 3: Downgrades are allowed, asking user for permission to uninstall the current app."
                 
                 dialog --backtitle 'Revancify' --defaultno \
-                    --yesno "The current app version $INSTALLED_VERSION is greater than the new version $APP_VER.\n\nDo you want to uninstall the current version and proceed with the downgrade?" 12 45
+                    --yesno "The current app version $INSTALLED_PATCHED_VERSION is greater than the new version $PATCHED_APP_VERSION.\n\nDo you want to uninstall the current version and proceed with the downgrade?" 12 45
 
                 if [ $? -eq 0 ]; then
                     log "Case 3: User agreed to uninstall for clean reinstall."
                     UNINSTALL_CURRENT_INSTALLATION=true
                 else
                     log "Case 3: User decided not to uninstall to continue the downgrade. Aborting..."
-                    notify msg "User declined to uninstall the current version.\n\nAborting installation...\n\nCopied patched $APP_NAME apk to Internal Storage..."
+                    notify msg "User declined to uninstall the current version.\n\nAborting installation...\n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
                     return 1
                 fi
             else
                 log "Case 3: Downgrades are not allowed, exiting."
-                notify msg "Downgrades are not allowed in Configuration, exiting.\n\nCopied patched $APP_NAME apk to Internal Storage..."
+                notify msg "Downgrades are not allowed in Configuration, exiting.\n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
                 return 1
             fi
         else
@@ -104,31 +126,31 @@ installAppRish() {
     fi
 
     if [ "$UNINSTALL_CURRENT_INSTALLATION" == true ]; then
-        notify info "Please Wait !!\nUninstalling $APP_NAME $APP_VER using Rish..."
+        notify info "Please Wait !!\nUninstalling $PATCHED_APP_APP_NAME using Rish..."
         if uninstallAppRish false true "$STORAGE"; then
             log "Uninstallation successful, proceeding with installation."
-            if ! rish -c "dumpsys package $PKG_NAME" 2>&1 | grep -q "Unable to find package"; then
+            if ! rish -c "dumpsys package $PATCHED_APP_PKG_NAME" 2>&1 | grep -q "Unable to find package"; then
                 log "Found hidden installation post uninstallation. This might be a different user."
                 HIDDEN_APP_INSTALL=true
             fi
         else
             log "Uninstallation failed."
-            mesage="Failed to uninstall the current app.\n\nAborting installation...\n\nCopied patched $APP_NAME apk to Internal Storage..."
+            mesage="Failed to uninstall the current app.\n\nAborting installation...\n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
             return 1
         fi
     fi
 
-    notify info "Please Wait !!\nInstalling $APP_NAME $APP_VER using Rish..."
+    notify info "Please Wait !!\nInstalling $PATCHED_APP_APP_NAME $PATCHED_APP_VERSION using Rish..."
 
     log "Attempting to install the patched APK..."
-    if bash system/rish-install.sh "$PKG_NAME" "$APP_NAME" "$EXPORTED_APK_NAME" "$STORAGE"; then
+    if bash system/rish-install.sh "$PATCHED_APP_PKG_NAME" "$PATCHED_APP_APP_NAME" "$EXPORTED_APK_NAME" "$STORAGE"; then
         log "Installation command executed successfully."
-        notify msg "$APP_NAME $APP_VER installed successfully using Rish!"
+        notify msg "$PATCHED_APP_APP_NAME $PATCHED_APP_VERSION installed successfully using Rish!"
     elif [ "$HIDDEN_APP_INSTALL" == true ] ; then
         # Second attempt to install the APK, if we had to uninstall the current app
         log "First installation attempt failed, trying again after uninstallation."
     else
-        log "Installation of $APP_NAME $APP_VER failed."
+        log "Installation of $PATCHED_APP_APP_NAME $PATCHED_APP_VERSION failed."
         notify msg "Installation Failed !!\nShare logs to developer."
         termux-open --send "$STORAGE/rish_log.txt"
         return 1
@@ -139,37 +161,37 @@ installAppRish() {
         log "Getting second attempt, this can happen in Cases 1, 2, 3, if we have multiple users in the device with the app..."
         
         dialog --backtitle 'Revancify' --defaultno \
-            --yesno "We coudn't install the App.\nA different user probably has an incompatible $APP_NAME app.\n\nDo you want to uninstall $APP_NAME from all users and proceed?\nWe cannot guarantee this will succeed..." 12 45
+            --yesno "We coudn't install the App.\nA different user probably has an incompatible $PATCHED_APP_APP_NAME app.\n\nDo you want to uninstall $PATCHED_APP_APP_NAME from all users and proceed?\nWe cannot guarantee this will succeed..." 12 45
         if [ $? -eq 0 ]; then
             log "User accepted to uninstall the app from all users."
             # We try to uninstall the app from all users, this can fail if the system doesn't allow it
-            notify info "Please Wait !!\nUninstalling $APP_NAME from all users using Rish..."
+            notify info "Please Wait !!\nUninstalling $PATCHED_APP_APP_NAME from all users using Rish..."
             if uninstallAppRish true true "$STORAGE"; then
                 log "Uninstallation from all users successful, proceeding with installation."
-                notify info "Please Wait !!\nInstalling $APP_NAME $APP_VER using Rish..."
-                if bash system/rish-install.sh "$PKG_NAME" "$APP_NAME" "$EXPORTED_APK_NAME" "$STORAGE"; then
+                notify info "Please Wait !!\nInstalling $PATCHED_APP_APP_NAME $PATCHED_APP_VERSION using Rish..."
+                if bash system/rish-install.sh "$PATCHED_APP_PKG_NAME" "$PATCHED_APP_APP_NAME" "$EXPORTED_APK_NAME" "$STORAGE"; then
                     log "Installation command executed successfully after uninstallation from all users."
-                    notify msg "$APP_NAME $APP_VER installed successfully using Rish!"
+                    notify msg "$PATCHED_APP_APP_NAME $PATCHED_APP_VERSION installed successfully using Rish!"
                 else
                     log "Installation failed after uninstallation from all users."
-                    notify msg "Installation Failed !!\nShare logs to developer. \n\nCopied patched $APP_NAME apk to Internal Storage..."
+                    notify msg "Installation Failed !!\nShare logs to developer. \n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
                     termux-open --send "$STORAGE/rish_log.txt"
                     return 1
                 fi
             else
                 log "Uninstallation from all users failed, aborting installation."
-                notify msg "Failed to uninstall the app from all users.\n\nAborting installation...\n\nCopied patched $APP_NAME apk to Internal Storage..."
+                notify msg "Failed to uninstall the app from all users.\n\nAborting installation...\n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
                 return 1
             fi
         else
             log "User declined to uninstall the app from all users, aborting installation."
-            notify msg "User declined to uninstall the app from all users.\n\nAborting installation...\n\nCopied patched $APP_NAME apk to Internal Storage..."
+            notify msg "User declined to uninstall the app from all users.\n\nAborting installation...\n\nCopied patched $PATCHED_APP_APP_NAME apk to Internal Storage..."
             return 1
         fi
     fi
     
     # If we reach this point, the installation was successful
-    log "Installation of $APP_NAME $APP_VER completed successfully, finalized code."
+    log "Installation of $PATCHED_APP_APP_NAME $PATCHED_APP_VERSION completed successfully, finalized code."
     if [ "$LAUNCH_APP_AFTER_MOUNT" == "on" ]; then
         # The su version used kill -9, I replaced it with the adb command 'am force-stop' avaliable in rish
         rish -c "settings list secure | sed -n -e 's/\/.*//' -e 's/default_input_method=//p' | xargs am force-stop && pm resolve-activity --brief $PKG_NAME | tail -n 1 | xargs am start -n && am force-stop com.termux"  &> /dev/null
@@ -188,17 +210,22 @@ uninstallAppRish() {
         rm "$STORAGE/rish_log.txt"
     fi
 
+    if [ -z "$PATCHED_APP_PKG_NAME" ]; then
+        log "PATCHED_APP_PKG_NAME is not set. Aborting uninstallation."
+        return 1
+    fi
+
 
     if [ "$UNINSTALL_FROM_ALL_USERS" = true ]; then
         log "Uninstalling from all users..."
-        if bash system/rish-uninstall.sh "$PKG_NAME" true "$STORAGE"; then
+        if bash system/rish-uninstall.sh "$PATCHED_APP_PKG_NAME" true "$STORAGE"; then
             return 0
         else
             return 1
         fi
     else
         log "Uninstalling from current user..."
-        if bash system/rish-uninstall.sh "$PKG_NAME" false "$STORAGE"; then
+        if bash system/rish-uninstall.sh "$PATCHED_APP_PKG_NAME" false "$STORAGE"; then
             return 0
         else
             return 1
--- system/rish-install.sh
+++ system/rish-install.sh
@@ -32,15 +32,15 @@ if [ "$(rish -c"[ -e $PATCHED_APP_PATH ] && echo Exists || echo Missing")" == "E
     log "Residual $PATCHED_APP_PATH deleted"
 fi
 
-log "Copying exported APK to /data/local/tmp/revancify..."
-rish -c "cp -f $EXPORTED_APP_PATH $PATCHED_APP_PATH"
+log "Moving exported APK to /data/local/tmp/revancify..."
+rish -c "mv -f $EXPORTED_APP_PATH $PATCHED_APP_PATH"
 
 if [ "$(rish -c "[ -e $PATCHED_APP_PATH ] && echo Exists || echo Missing")" == "Missing" ]; then
     log "Failed to move patched APK to $PATCHED_APP_PATH"
     exit 1
 fi
 
-CMD_RISH="pm install --user current $PATCHED_APP_PATH"
+CMD_RISH="pm install -r --user current $PATCHED_APP_PATH"
 
 # We execute the install command using rish
 OUTPUT=$(rish -c "$CMD_RISH" 2>&1)
@@ -51,24 +51,14 @@ log "Install output: $OUTPUT"
 if echo "$OUTPUT" | grep -q "^Success"; then
     log "Install succeeded."
     rish -c "rm -f $PATCHED_APP_PATH"  # Clean up the temporary APK
-    if [ -z "$STORAGE" ]; then
-        rish -c "rm -f $EXPORTED_APP_PATH"  # Clean up the exported APK on success
-    else
-        rm -f "$STORAGE/Patched/$EXPORTED_APK_NAME.apk"  # Clean up the exported APK on success
-    fi
     exit 0
 elif [ "$(rish -c "pm list packages --user current | grep -q $PKG_NAME && echo Installed")" == "Installed" ]; then
     log "Install succeeded, but output was not 'Success'."
     rish -c "rm -f $PATCHED_APP_PATH"  # Clean up the temporary APK
-    if [ -z "$STORAGE" ]; then
-        rish -c "rm -f $EXPORTED_APP_PATH"  # Clean up the exported APK on success
-    else
-        rm -f "$STORAGE/Patched/$EXPORTED_APK_NAME.apk"  # Clean up the exported APK on success
-    fi
     exit 0
 else
     # Sometimes the output is not "Success" but still the app is installed, so we check for that.
-    log "Install failed."
-    rish -c "rm -f $PATCHED_APP_PATH"  # Clean up the temporary APK
+    log "Install failed. Moving APK back to original location."
+    rish -c "mv -f $PATCHED_APP_PATH $EXPORTED_APP_PATH"  # Move the APK back to the original location
     exit 1
 fi
